f211b56c225481385737919e8ec2b58a1aa886cb
Niti Nrw <niti.patel@flatironschool.com>
2016-11-06 10:20:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
